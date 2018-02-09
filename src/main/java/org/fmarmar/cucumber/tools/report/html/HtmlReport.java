bfaa54b03ae452d795154ea1e5b9f3301704781b
fmarmar <fmarmar@users.noreply.github.com>
2018-02-09 11:22:49 +0100

    Get Metadata from command line

--- src/main/java/org/fmarmar/cucumber/tools/report/html/HtmlReport.java
+++ src/main/java/org/fmarmar/cucumber/tools/report/html/HtmlReport.java
@@ -20,34 +20,59 @@ import org.fmarmar.cucumber.tools.rerun.RerunFileConverter;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
-@Parameters(commandNames = "html-report", commandDescription = "")
+@Parameters(
+		commandNames = "html-report", 
+		commandDescription = "Generate an html report from the given json reports"
+		)
 public class HtmlReport implements Command {
 
 	public static final int DEFAULT_THREADS_SIZE = 100;
 
 	public static final Path DEFAULT_OUTPUT = Paths.get("reports", "html");
 	
-	@Parameter(names = { "--reports", "-r" }, variableArity = true, description = "", converter = PathConverter.class)
+	public static final String DEFAULT_PROJECT_NAME = "No project";
+
+	@Parameter(
+			names = { "--reports", "-r" }, 
+			variableArity = true, 
+			description = "Path(s) where to find the json reports", 
+			converter = PathConverter.class
+			)
 	private List<Path> reports;
-	
-	@Parameter(names = { "--output", "-o" }, description = "", converter = RerunFileConverter.class)
+
+	@Parameter(
+			names = { "--output", "-o" }, 
+			description = "Path where the report will be generated. Default: reports/html", 
+			converter = RerunFileConverter.class)
 	private Path output = DEFAULT_OUTPUT;
 	
+	@Parameter(
+			names = { "--project", "-p" }, 
+			description = "Project name to print in the report. Default: " + DEFAULT_PROJECT_NAME, 
+			converter = RerunFileConverter.class)
+	private String projectName = DEFAULT_PROJECT_NAME;
+	
+	@Parameter(
+			names = { "--build", "-b" }, 
+			description = "Build id to print in the report. Default: (empty)", 
+			converter = RerunFileConverter.class)
+	private String buildId = null;
+
 	private ReportParser parser;
-	
+
 	private ReportGenerator generator;
-	
+
 	@Override
 	public void initialize() {
 		checkOutput();
-		
+
 		try {
 			init();
 		} catch (IOException e) {
 			throw new CommandException(e.getMessage(), e);
 		}
 	}
-	
+
 	private void checkOutput() {
 
 		File outputFile = output.toFile();
@@ -63,27 +88,27 @@ public class HtmlReport implements Command {
 		}
 
 	}
-	
+
 	private void init() throws IOException {
 		parser = new ReportParser();
-		ReportMetadata reportMetadata = new ReportMetadata(); // TODO build
+		ReportMetadata reportMetadata = new ReportMetadata(projectName, buildId); // TODO build
 		PageGenerator pageGenerator = new VelocityPageGenerator(reportMetadata, DEFAULT_THREADS_SIZE);
 		generator = new ReportGenerator(pageGenerator, output, DEFAULT_THREADS_SIZE);
 	}
 
 	@Override
 	public void run() {
-		
+
 		try {
 			ParsedReports parsedReports = parser.parse(reports);
-			
+
 			generator.prepareReport(parsedReports.getEmbeddingsDirectory());
 			generator.generateReport(parsedReports.getFeatures());
 			generator.finishReport();
-			
+
 		} catch (IOException | InterruptedException e) {
 			throw new CommandException(e.getMessage(), e);
 		}
 	}
-	
+
 }
--- src/main/java/org/fmarmar/cucumber/tools/report/html/support/ReportMetadata.java
+++ src/main/java/org/fmarmar/cucumber/tools/report/html/support/ReportMetadata.java
@@ -3,21 +3,30 @@ package org.fmarmar.cucumber.tools.report.html.support;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.commons.lang.StringUtils;
-
 import lombok.Data;
 
 @Data
 public class ReportMetadata {
 	
-	private String projectName = StringUtils.EMPTY;
+	private final String projectName;
 	
-	private String buildId;
+	private final String buildId;
 	
-	private final String buildTimestamp = formatTimestamp(new Date());
+	private final String buildTimestamp;
 	
+	public ReportMetadata() {
+		this(null, null);
+	}
+	
+	public ReportMetadata(String projectName, String buildId) {
+		this.projectName = projectName;
+		this.buildId = buildId;
+		this.buildTimestamp = formatTimestamp(new Date());
+	}
+
 	private static String formatTimestamp(Date date) {
 		return new SimpleDateFormat("dd MMM yyyy, HH:mm").format(date);
 	}
+	
 	
 }
