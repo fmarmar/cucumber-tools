a7bd19dadb1a0cd2a813f92be04382902689cb3e
fmarmar <fmarmar@users.noreply.github.com>
2018-02-21 14:29:51 +0100

    Fix metadata tooltip formatting

--- src/main/java/com/github/fmarmar/cucumber/tools/report/html/page/velocity/EscapeHtmlReference.java
+++ src/main/java/com/github/fmarmar/cucumber/tools/report/html/page/velocity/EscapeHtmlReference.java
@@ -10,6 +10,7 @@ import org.owasp.html.PolicyFactory;
  * Escapes all html and xml that was provided in a reference before inserting it into a template.
  *
  * References that start with $_sanitize_ will be sanitized to allow urls.
+ * REferences from TemplateUtils methods ($utils.) will not be escaped
  */
 public final class EscapeHtmlReference implements ReferenceInsertionEventHandler {
 
@@ -25,8 +26,12 @@ public final class EscapeHtmlReference implements ReferenceInsertionEventHandler
             return null;
         }
 		
-		if(reference.startsWith("$_sanitize_")) {
+		if (reference.startsWith("$_sanitize_")) {
             return LINKS.sanitize(value.toString());
+        }
+		
+		if (reference.startsWith("$utils.")) {
+            return value.toString();
         }
 		
 		return StringEscapeUtils.escapeHtml4(value.toString());
